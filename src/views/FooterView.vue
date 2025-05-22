f73ebee8a808d8c41df71a42380c8c2798e4fe53
Bartek Pierr <bartek.pierr@gmail.com>
2025-05-23 00:36:18 +0200

    create-footer-component

---
+++ src/views/FooterView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+    <h1>Zapraszamy do kontaktu! MS Studio - Meble na miarę.</h1>
+  </div>
+</template>
+
+<style>
+  .footer {
+    min-height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
