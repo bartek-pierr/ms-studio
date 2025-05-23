a41a6fef514c273301fa79136af548736c440fd3
Bartek Pierr <bartek.pierr@gmail.com>
2025-05-23 09:30:40 +0200

    quickfix

--- src/views/SoundDeliveryView.vue
+++ src/views/SoundDeliveryView.vue
@@ -5,12 +5,22 @@ import InfoIcon from '../components/icons/IconInfo.vue'
 <template>
   <div class="soundDelivery">
     <div class="warrningBox">
-      <span class="warrningIcon">
-        <InfoIcon fill="rgb(238, 161, 18)" />
-      </span>
-      <p class="warrningParagraph">
-        Przepraszamy, strona jest dalej w budowie. Wkrótce będzie dostępna pełna wersja, a tymczasem niektóre funkcjonalności mogą działać nieprawidłowo.
-      </p>
+      <div class="warrningBox__wrapper">
+        <span class="warrningIcon">
+          <InfoIcon fill="rgb(238, 161, 18)" />
+        </span>
+        <p class="warrningParagraph">
+          Przepraszamy, strona jest dalej w budowie. Wkrótce będzie dostępna pełna wersja, a tymczasem niektóre funkcjonalności mogą działać nieprawidłowo. Zapraszamy do kontaktu mailowego lub telefonicznego.
+        </p>
+        <div class="warrningBox__contact">
+        <p class="warrningParagraph">
+          Mail<br ><a href="mailto:kontakt@ms-studio.info.pl"><span>kontakt@ms-studio.info.pl</span></a>
+        </p>
+        <p class="warrningParagraph">
+          Telefon<br ><a href="tel:691 806 875"><span>691 806 875</span></a>
+        </p>
+      </div>
+      </div>
     </div>
     <h1>Już wkrótce!</h1>
   </div>
