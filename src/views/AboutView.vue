1ddc49ffd13441bf9541fd1f8924c23ddded6364
Bartek Pierr <bartek.pierr@gmail.com>
2025-05-23 05:17:15 +0200

    create-ms-studio-view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,54 @@
 <template>
+  <h1 class="headerMain">Oferta dla klienta indywidualnego</h1>
   <div class="about">
-    <h1>Strona - MS Studio</h1>
+    <div class="about__item">
+      <h2 class="headerSecond">
+        OD POMYSŁU DO REALIZACJI
+      </h2>
+      <p>
+        Krok po kroku razem z Klientem tworzymy kuchnię, łazienkęczy garderobę marzeń. Uwzględniamy indywidualny styl Klienta.Bierzemy pod uwagę funkcjonalność, jakość oraz estetykę projektowanychzabudowań.
+      </p>
+      <p>
+        Zaprojektowane zabudowy możemy wyposażyć w sprzęt AGDz 5 letnią gwarancją!
+      </p>
+      <p>
+        Dostarczamy również szczegółowe informacje dotyczące przygotowania pomieszczeń firmom budowlanym oraz wykonawcom hydrauliki czy elektryki.
+      </p>
+    </div>
+    <div class="about__item">
+      <h2 class="headerSecond">
+        MEBLE NA MIARĘ POTRZEB I MOŻLIWOŚCI
+      </h2>
+      <p>
+        Niezależnie od posiadanych przez Klienta funduszy oraz przestrzeni, jaką dysponuje, wspólnie tworzymy projekt zabudowy meblowej o wysokiej estetyce, funkcjonalności i jakości wykonania. Wystarczy zapytać o cenę, aby przekonać się, że meble na wymiar są dostępne dla każdego.
+      </p>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100%;
+}
+
+.about__item {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  gap: 8px;
+  margin-bottom: 32px;
+}
+
+.about__item p {
+  margin-bottom: 8px;
+}
+
+h1 {
+  margin-bottom: 30px;
 }
 </style>
